d0a5291fecd1871aec736d68c432f82ad0b34380
BhavyaShankar <bhavyashankar18@gmail.com>
2025-08-02 11:44:29 -0400

    Fleetrender

--- FleetManagementMicroservice/models/userModel.js
+++ FleetManagementMicroservice/models/userModel.js
@@ -1,6 +1,10 @@
 // models/userModel.js
 const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
+
+
+
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
