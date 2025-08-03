f8ddf725d1b94c04c035c5035092496df19cbc83
BhavyaShankar <bhavyashankar18@gmail.com>
2025-08-03 09:12:49 -0400

    commit

---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+// models/userModel.js
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  age: Number,
+});
+
+module.exports = mongoose.model('User', userSchema);
